682f75199f589b8a6364462fb0caa62c44f7788d
Turbobot <turbobot@vercel.com>
2024-04-26 20:21:34 +0530

    feat(create-turbo): apply official-starter transform

--- backend/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-
-const router = Router()
-
-// router.use('/user',userRouter)
-// router.use('/account',accountRouter)
-
-export default router
--- backend/src/utils/statusCode.ts
+++
@@ -1,7 +0,0 @@
-export enum responseStatus{
-    success=200,
-    incorrectInput=401,
-    internalServerError=500,
-    unauthorized=403
-
-}
